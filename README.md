e39e89cb90122a91ec460bb315aaac1f8b209460
Kinjal Parmar <kinjalparmar1992@gmail.com>
2016-12-26 17:24:31 +0530

    Modified files

--- README.md
+++ README.md
@@ -7,11 +7,19 @@ This is the submission for Udacity's Classic Arcade Game Project for the **Front
 ### Princess Ema's Story:
 
 Meet the cute little princess, Ema. Unfortunately, Ema's parents have been cursed by a cruel witch. In order to break the curse and kill the witch, the princess have to collect 1 litre water from the Divine Lake. That Divine Lake is situated in the witch's kingdom which is surrounded by enemy gurads- The Bugs.The Bugs move in varying speeds. 
-#### Game Rules :
 
+##### How To Run:
+To start playing the game, clone the project or download the zip folder. Then open the index.html file in any modern browser.
+
+##### Goal :
+Collect 1000ml of divine water.
+
+##### How to play :
 * The princess can move left, right, up and down with the help of arrow-keys.
 * Each time when she reaches the lake, she can collect 100ml of water.
-* On colliding with Bug, she will loose 50ml of her collected water and have to start from the beginning.
+
+##### Beware :
+* On colliding with Bug, princess loose 50ml of her collected water and have to start from the beginning.
 * Ema gets only three chances to fight with the bugs. She will be imprisioned after the third trial and cannot continue the mission.
 
 **Help Ema to collect divine water by avoiding the Bugs.** 
--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// Initializing the score and life variables */
-var life= 3;
-var score=0;
-var greet;
-
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
@@ -23,7 +18,6 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += dt * this.speed;
-	// to make sure that vechicles start with the beginning after crossing the playong area
     if (this.x > 500) {
         this.x = 1;
     }
@@ -42,40 +36,33 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var Player = function (x, y) { //player class
+var Player = function (x, y, score, life, greet) { //player class
     this.sprite = "images/char-princess-girl.png";
     this.x = 200;
     this.y = 400;
-	
-	/* displaying the initial score and life of player */
-	display();
-	console.log("life left" +life);
-	console.log("score" +score);	
+    this.life= 3;
+    this.score=0;
+	console.log("score" + " " + this.score);
+    console.log("life left" + " " + this.life);
     //this.speed = 100;
 };
-//Setting the rule for update- start from beginning
+
+//Set rule if player is within 20, the player must reset to beginning
 Player.prototype.update = function (dt) {
-	// check for the collision
     this.checkCollisions();
-	/*if player reach the water tile i.e top */
     if (this.y < 20) {
         this.reset();
-		// if the player reach at top, she will score 100 points
-        score+=100;
-		// displaying the updated score
-        display();
-        console.log(score);
+        this.score+= 100;
+        this.display();
+        console.log("score" + " " + this.score);
     }
-	// if the player scores 1000 points then she wins
-    if(score>=1000){
-        greet="Congratulations you win!!";
-		// dislay modal
-        modalShow();
-		//reset score and life to their initial values
-        score_reset();
+    if(this.score>= 1000){
+        this.greet="Congratulations you win!!";
+		console.log("Congratulations you win");
+        this.modalShow();
+		this.score_reset();
     }
 };
-
 
 var player = new Player();
 
@@ -84,7 +71,100 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Declaring an array called allEnemies and placing all enemy objects inside it
+// to display the score and life left
+Player.prototype.display = function() {
+	$("#score_points").text(" " + this.score + "ml");
+    $("#life_left").text(" " + this.life);
+};
+// to reset the score nd life left
+Player.prototype.score_reset = function () {
+    this.score=0;
+    this.life=3;
+    this.display();
+	console.log("New Game")
+	console.log("score" + " " + this.score);
+    console.log("life left" + " " + this.life);
+};
+// to display the modal
+Player.prototype.modalShow = function () {
+    $("#modal-game").removeClass("display-hide"); // Not to display the present div
+    $("#modal-game").addClass("display-modal");
+	console.log("Your score is" + " " + this.score);
+    $("#score_points_modal").text("You have collected:"+" "+this.score+"ml");
+    $("#greeting_msg").text(this.greet);
+    $("#buttonOK").click(function () {
+        $("#modal-game").removeClass("display-modal");
+        $("#modal-game").addClass("display-hide");
+    });
+};
+//Setting width and height for the gameboard tiles
+var TILE_WIDTH = 101;
+var TILE_HEIGHT = 83;
+
+//For each keystroke, the player shall move along the x and y axis
+//Set variable to less than 100 for up and down to keep player inside game tile
+Player.prototype.handleInput = function (code) {
+    switch (code) {
+        case "up":
+            if (this.y > 0) {
+                this.y -= TILE_HEIGHT;
+            }
+            break;
+
+        case "down":
+            if (this.y < 400) {
+                this.y = this.y + TILE_HEIGHT;
+            }
+            break;
+
+        case "left":
+            if (this.x > 0) {
+                this.x = this.x - TILE_WIDTH;
+            }
+            break;
+
+        case "right":
+            if (this.x < 400) {
+                this.x += TILE_WIDTH;
+            }
+            break;
+    }
+};
+
+//reset or start over function to staring position
+Player.prototype.reset = function () {
+    this.x = 200;
+    this.y = 400;
+};
+
+//Set all collisons with player to make player reset game
+Player.prototype.checkCollisions = function () {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (allEnemies[i].x + 40 > this.x && this.x + 40 > allEnemies[i].x && allEnemies[i].y + 50 > this.y && this.y + 50 > allEnemies[i].y) {
+            this.reset();
+			console.log("Collision");
+            this.score-= 50;
+            this.display();
+            if(this.score< 0){
+                this.score= 0;
+                this.display();
+            }
+			console.log("score" + " " + this.score);
+            this.life-=1;
+            this.display();
+            console.log("life left" + " " + this.life);
+            if(this.life<=0){
+				console.log("Game Over");
+                this.display();
+                this.greet="Nice try!!!";
+                this.modalShow();
+				this.score_reset();
+            }
+        }
+    }
+};
+
+// Place all enemy objects in an array called allEnemies
 // Now instantiate your objects.
 //Set enemies initial coordinates and speed (x, y, speed)
 var allEnemies = [];
@@ -103,98 +183,6 @@ allEnemies.push(enemy);
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-//Setting width and height for the gameboard tiles
-var BlockWidth = 101;
-var BlockHeight = 90;
-
-//For each keystroke, the player shall move along the x and y axis
-//Set variable to less than 100 for up and down to keep player inside game tile
-Player.prototype.handleInput = function (code) {
-    switch (code) {
-        case "up":
-            if (this.y > 0) {
-                this.y -= BlockHeight;
-            }
-            break;
-
-        case "down":
-            if (this.y < 400) {
-                this.y = this.y + BlockHeight;
-            }
-            break;
-
-        case "left":
-            if (this.x > 0) {
-                this.x = this.x - BlockWidth;
-            }
-            break;
-
-        case "right":
-            if (this.x < 400) {
-                this.x += BlockWidth;
-            }
-            break;
-    }
-};
-
-//reset or start over function to staring position
-Player.prototype.reset = function () {
-    this.x = 200;
-    this.y = 400;
-};
-//Set all collisons with player to make player reset game
-Player.prototype.checkCollisions = function () {
-    for (var i = 0; i < allEnemies.length; i++) {
-        if (allEnemies[i].x + 40 > this.x && this.x + 40 > allEnemies[i].x && allEnemies[i].y + 50 > this.y && this.y + 50 > allEnemies[i].y) {
-            //if collision occurs reset the player
-			this.reset();
-			//reduce the score on collision
-            score-= 50;
-			console.log("score" +score);
-			//reduce the life by 1
-            life-=1;
-			console.log("life left" +life);
-			//display the reduced score and life
-            display();
-			//check if the score is negative, if so then make it equals to zero
-            if(score<0){
-                score= 0;
-                display();
-            }
-			//if life-left equals zero then stop the game, display the score and reset 
-            if(life<=0){
-                display();
-                greet="Nice try!!!";
-                modalShow();
-                console.log("score" +score);
-                score_reset();
-            }
-        }
-    }
-};
-
-// display the score and life-left
-function display() {
-    $("#score_points").text(score+"ml");
-    $("#life_left").text(" "+life);
-}
-// resetting the score to initial values and display them.
-function score_reset(){
-    score=0;
-    life=3;
-    display();
-}
-
-function modalShow(){
-    $("#modal-game").removeClass("display-hide"); // Not to display the present div
-    $("#modal-game").addClass("display-modal");
-    $("#score_points_modal").text("You have collected:"+" "+score+"ml");
-    $("#greeting_msg").text(greet);
-    $("#buttonOK").click(function () {
-    $("#modal-game").removeClass("display-modal");
-    $("#modal-game").addClass("display-hide");
-});
-}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
